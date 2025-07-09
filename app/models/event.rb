9768fd994bf53c8befa0088820d4b0111a5e547c
Adam Geltz <ageltz07@gmail.com>
2025-07-09 15:42:41 -0400

    Added QR Code generation logic and now saving as png locally with active storage

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   has_many :items
+  has_one_attached :qr_image
 
+  after_commit :create_qr_code, on: [:create, :update], unless: :updating_qr_code_only?
 
   private
 
@@ -8,6 +10,19 @@ class Event < ApplicationRecord
     # https://github.com/whomwah/rqrcode
     require "rqrcode"
 
-    qr = RQRCode::QRCode.new(Rails.application.routes.url_helpers.event_url(self, host: 'localhost:3000'))
+    qr_code = RQRCode::QRCode.new(Rails.application.routes.url_helpers.event_url(self, host: "localhost:3000"))
+    qr_png = qr_code.as_png
+    qr_image.attach(
+      io: StringIO.new(qr_png.to_s),
+      filename: "event_#{id}_qr.png",
+      content_type: "image/png"
+    )
+  end
+
+  def updating_qr_code_only?
+    # This is to avoid an infinite loop of callbacks when generating the qr code using an after_commit
+    # callback.
+
+    qr_image.attached? && previous_changes.keys == []
   end
 end
